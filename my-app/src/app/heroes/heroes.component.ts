bbc04a9c3d4236b39e3dcb980888ee2f2aa19486
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-02 14:25:51 +0100

    Third commit wit white screen of death

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -8,11 +8,12 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+/*  hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  }
+  }*/
   heroes = HEROES;
   selectedHero: Hero;
 
